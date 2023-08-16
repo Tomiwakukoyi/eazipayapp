eaf085437fea3a39d5ad25c9b74062fc67904c99
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2023-08-16 01:41:31 +0100

    First commit after skeletal setup for eazipay

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      logo
+      <div> Indivdal Business pricing set your payroll</div>
+      <div>login button signup button</div>
+    </div>
+  );
+};
+
+export default Navbar;
